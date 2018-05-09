5bbf9ef27e322b643dbf4543beaf144be548dd44
seita <seita.1224.ds@outlook.com>
2018-05-09 23:12:58 +0900

    プロジェクト立ち上げ

--- azure_test/app/__init__.py
+++ azure_test/app/__init__.py
@@ -4,18 +4,4 @@
 __init__.py
 flask app
 """
-__author__ = 'Yoshiya Ito <myon53@gmail.com>'
-__version__ = '0.0.1'
-__date__ = '30 03 2016'
 
-from flask import Flask
-from flask.ext.mongoengine import MongoEngine
-from flask.ext.restful import Api
-from redis import Redis
-
-sample = Flask(__name__)
-redis = Redis()
-mongo = MongoEngine(sample)
-api = Api(sample)
-
-import app.views
